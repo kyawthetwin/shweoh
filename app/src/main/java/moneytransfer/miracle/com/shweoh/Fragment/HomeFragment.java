a4a25c0c3d22f1433703d639de586d07c1a2aed6
Kyaw Thet Win <kyawthetwin@aceplussolutions.com>
2017-05-23 11:20:11 +0630

    Fixed Main UI

--- app/src/main/java/moneytransfer/miracle/com/shweoh/Fragment/HomeFragment.java
+++ app/src/main/java/moneytransfer/miracle/com/shweoh/Fragment/HomeFragment.java
@@ -2,10 +2,13 @@ package moneytransfer.miracle.com.shweoh.Fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import moneytransfer.miracle.com.shweoh.Adapter.MainPagerAdapter;
 import moneytransfer.miracle.com.shweoh.R;
 
 /**
@@ -22,6 +25,7 @@ public class HomeFragment extends android.support.v4.app.Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
@@ -29,6 +33,31 @@ public class HomeFragment extends android.support.v4.app.Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tab_layout_main);
+        tabLayout.addTab(tabLayout.newTab().setText("Tutorials"));
+        tabLayout.addTab(tabLayout.newTab().setText("Courses"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.mainPager);
+        final MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(getFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(mainPagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         // Inflate the layout for this fragment
         return rootView;
--- app/src/main/java/moneytransfer/miracle/com/shweoh/MainActivity.java
+++ app/src/main/java/moneytransfer/miracle/com/shweoh/MainActivity.java
@@ -2,23 +2,18 @@ package moneytransfer.miracle.com.shweoh;
 
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import moneytransfer.miracle.com.shweoh.Adapter.MainPagerAdapter;
 import moneytransfer.miracle.com.shweoh.Fragment.FragmentDrawer;
 import moneytransfer.miracle.com.shweoh.Fragment.FriendsFragment;
 import moneytransfer.miracle.com.shweoh.Fragment.HomeFragment;
 import moneytransfer.miracle.com.shweoh.Fragment.MessagesFragment;
-import moneytransfer.miracle.com.shweoh.Adapter.MainPagerAdapter;
-import moneytransfer.miracle.com.shweoh.Adapter.PagerAdapter;
 
 public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener  {
 
@@ -36,33 +31,6 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         setContentView(R.layout.activity_main);
 
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout_main);
-        tabLayout.addTab(tabLayout.newTab().setText("Tutorials"));
-        tabLayout.addTab(tabLayout.newTab().setText("Courses"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.mainPager);
-        final MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(mainPagerAdapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setSupportActionBar(mToolbar);
@@ -72,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+        displayView(0);
 
     }
 
